d66fe062fe3f96644b6feabeefc951db4706cf9c
xlxshx <168596940+xlxshx@users.noreply.github.com>
2024-05-17 13:58:51 +1200

    Add files via upload

---
+++ P_02_variables.py
@@ -0,0 +1,17 @@
+# Code to learn about variables
+
+# #Variables can be a string (Note the speech marks)
+greeting = "Hello World"
+
+# Variables can be a number
+fav_num = 7
+
+# Variables can be Boolean (True/False)
+love_programming = True
+
+print(greeting)
+print(fav_num)
+print(love_programming)
+
+# This variable has a problem (I now have fixed the problem)
+my_name = "Miss G"
---
+++ welcome.py
@@ -0,0 +1,7 @@
+# Hello World Program
+# Author: Elisha Cristobal
+# Date: 12 April 2024
+# Version: 1.0
+
+# Hello World Program
+print("Hello World!")
